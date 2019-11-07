453e88d1874725009cca65f1ae1011685df0f6db
shhj1998 <choseunghyek@gmail.com>
2019-11-07 17:44:18 +0900

    Bad read fix

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -148,7 +148,7 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
-  if (fault_addr == NULL || user)
+  if (not_present || user)
     exit (-1);
 
   /* To implement virtual memory, delete the rest of the function
